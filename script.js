04c2f1b92265a02b610b21c022cef91b633ab478
owen-mcg <70607067+owen-mcg@users.noreply.github.com>
2022-06-30 21:13:55 -0500

    html, css edits for ux

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function compute()
-{
+function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
-    //var amount = principal *
     var year = new Date().getFullYear() + parseInt(years);
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\</br\>at an interest rate of " + rate + ",\</br\>you will receive " + interest + " " + year;
+    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\</br\>at an interest rate of " + rate + "%,\</br\>you will receive an amount of " + interest + ",\<br\>in the year " + year;
 }
 
 function updateRate()
